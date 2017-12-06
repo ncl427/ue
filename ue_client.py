f94ace18999a1e75dda4454c9ce9ce585cbbf9a2
Muhammad Afaq <afaq24@gmail.com>
2017-12-06 18:48:35 +0900

    logging-work

--- ue_client.py
+++ ue_client.py
@@ -1,9 +1,12 @@
 from bjsonrpc import connect
 
 # connecting to vBBU
+print "\n---------------------"
+print "...connecting ue-vBBU"
 c = connect("192.168.186.61")
-print "ue-connecting to vBBU"
+print "connected ue-vBBU."
 
 # sending attach request to vBBU with a service-type for video
+print "...attaching ue-vBBU"
 response = c.call.attachvBBU("192.168.186.51", 1)
-print "vBBU-response: ", response
+print "attaching ue-vBBU response: ", response
