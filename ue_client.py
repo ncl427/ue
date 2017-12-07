c2aee3a41298d1698102837dfb04fa7ba1c2176f
Muhammad Afaq <afaq24@gmail.com>
2017-12-07 20:28:25 +0900

    added ueAttachClass

--- ue_client.py
+++ ue_client.py
@@ -1,6 +1,13 @@
 from bjsonrpc import connect
 import pickle
 from models.Mdd import Mdd
+from models.ueAttachObj import ueAttachObj
+
+def createAttachObj():
+   obj = ueAttachObj()
+   obj.ip = "192.168.186.51"
+   #obj.serviceType = 4
+   return obj
 
 # connecting to vBBU
 print "\n---------------------"
@@ -9,8 +16,12 @@ c = connect("192.168.186.61")
 print "connected ue-vBBU."
 
 # sending attach request to vBBU with a service-type for video
+attachedObj = createAttachObj()
+print "ueAttachObj:"
+print "\t", attachedObj.ip
+print "\t", attachedObj.serviceType
 print "...attaching ue-vBBU"
-response = c.call.attachvBBU("192.168.186.51", 4)
+response = c.call.attachvBBU("192.168.186.51", 1)
 print "attaching ue-vBBU response: "
 responseUnpickled = pickle.loads(response)
 print "\tMdd-nesId: ", responseUnpickled.nesId
