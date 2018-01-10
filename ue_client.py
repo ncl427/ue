92cf3ffbb38c4c286db6c5132f0f60d48fbac6a0
Muhammad Afaq <afaq24@gmail.com>
2018-01-10 11:48:54 +0900

    last updated

--- ue_client.py
+++ ue_client.py
@@ -2,11 +2,12 @@ from bjsonrpc import connect
 import pickle
 from models.Mdd import Mdd
 from models.ueAttachObj import ueAttachObj
+import time
 
 def createAttachObj():
    obj = ueAttachObj()
    obj.ip = "192.168.186.51"
-   #obj.serviceType = 4
+   obj.serviceType = 5
 
    #print "ueAttachObj:"
    #print "\t", obj.ip
@@ -16,17 +17,23 @@ def createAttachObj():
 # connecting to vBBU
 print "\n---------------------"
 print "...connecting ue-vBBU"
+time.sleep(1)
 c = connect("192.168.186.61")
 print "connected ue-vBBU."
+time.sleep(1)
 
 # sending attach request to vBBU with a service-type for video
 
 attachedObj = createAttachObj()
 attachedParameter = pickle.dumps(attachedObj)
-print "...attaching ue-vBBU"
+print "...attaching ue-vBBU-..."
+time.sleep(2)
 response = c.call.attachvBBU(attachedParameter)
-
-print "attaching ue-vBBU response: "
 responseUnpickled = pickle.loads(response)
+
+time.sleep(1)
+print "attached ue-vBBU-..."
+time.sleep(0.5)
+print "attaching ue-vBBU-... response: "
 print "\tMdd-nesId: ", responseUnpickled.nesId
 print "\tMdd-tempId: ", responseUnpickled.tempId
